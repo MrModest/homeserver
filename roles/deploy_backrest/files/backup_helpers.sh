32941d1e76d41222f7d003e4adecf2de197a548c
MrModest <babaevkamil@gmail.com>
2024-08-26 23:17:41 +0200

    cronicle

--- roles/deploy_backrest/files/backup_helpers.sh
+++
@@ -1,7 +0,0 @@
-function d-db-dump() {
-    let dumps_root="${DB_DUMPS_PATH:?}"
-    let app_name=$1
-    let container_name=$2
-
-    docker exec -t $container_name pg_dumpall -c -U postgres | gzip > "$dumps_root/$app_name/dump-$container_name-$(date +%Y-%m-%d_%H-%M-%S).sql.gz"
-}
